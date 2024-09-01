28d5dbb0b692f47cc87c238e93f818401ebbe58f
fjchen7 <fjchen7@gmail.com>
2024-09-01 16:27:31 +0800

    env: 分配栈和清零.bss段

--- os/src/entry.asm
+++ os/src/entry.asm
@@ -5,6 +5,19 @@
     # 声明全局符号_start，作为程序的入口。
     .globl _start
 _start:
-    # 将立即数100存入寄存器x1。
-    # li是指令的名字，全称Load Immediate。
-    li x1, 100
+    # 将boot_stack_top的地址存入sp寄存器。sp寄存器表示指向栈顶的指针。
+    # la指令的全称是Load Address，用于将地址存入寄存器。
+    la sp, boot_stack_top
+    # 调用内核的入口函数rust_main，将控制权交给Rust代码。
+    call rust_main
+
+    # 声明段.bss.stack。该段会被链接脚本linker.ld放入.bss段中。
+    .section .bss.stack
+    # 声明全局符号boot_stack_lower_bound，表示栈的底部（内存高位）。
+    .globl boot_stack_lower_bound
+boot_stack_lower_bound:
+    # 为栈预留空间 4096 * 16B = 64KB
+    .space 4096 * 16
+    # 声明全局符号boot_stack_top，表示栈的顶部（内存低位）。
+    .globl boot_stack_top
+boot_stack_top:
--- os/src/main.rs
+++ os/src/main.rs
@@ -7,4 +7,27 @@ use core::arch::global_asm;
 // 载入汇编代码
 global_asm!(include_str!("entry.asm"));
 
-fn main() {}
+// 不同的编译器，在编译时，会修改函数/变量的符号名，以做到解决命名冲突或增加类型安全等。
+// 这叫做name mangling，不同的编译器有自身的策略。
+//
+// #[no_mangle]的作用：告诉编译器不修改函数的符号（name mangling）
+// 我们要在汇编代码里调用Rust函数，因此要确保函数符号不被修改。否则，汇编代码将找不到该函数。
+#[no_mangle]
+pub fn rust_main() -> ! {
+    // 内核初始化
+    clear_bss();
+    loop {}
+}
+
+// 清零bss段（未初始化的全局变量）
+fn clear_bss() {
+    // 从链接器脚本（linker.ld）获取内存段的地址。通过引用外部的C函数来获取它们。
+    extern "C" {
+        fn sbss(); // .bss段的起始地址
+        fn ebss(); // .bss段的结束地址
+    }
+    // 将.bss段清零
+    (sbss as usize..ebss as usize).for_each(|a| unsafe {
+        (a as *mut u8).write_volatile(0)
+    })
+}
