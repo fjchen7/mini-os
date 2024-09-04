0db741f1087423cf70e99a5e2d0e825c5e22ce08
fjchen7 <fjchen7@gmail.com>
2024-09-04 23:52:44 +0800

    multipro: 修改Trap处理模块

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -1,6 +1,6 @@
 mod context;
 
-use crate::{batch::run_next_app, syscall::syscall};
+use crate::{syscall::syscall, task::TASK_MANAGER};
 use core::arch::global_asm;
 use riscv::register::{
     mtvec::TrapMode,
@@ -42,12 +42,12 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
         // 访存异常
         Trap::Exception(Exception::StoreFault) | Trap::Exception(Exception::StorePageFault) => {
             println_kernel!("PageFault in application, kernel killed it.");
-            run_next_app();
+            TASK_MANAGER.exit_current_and_run_next();
         }
         // 非法指令
         Trap::Exception(Exception::IllegalInstruction) => {
             println_kernel!("IllegalInstruction in application, kernel killed it.");
-            run_next_app();
+            TASK_MANAGER.exit_current_and_run_next();
         }
         // 暂时不支持的Trap类型
         _ => {
