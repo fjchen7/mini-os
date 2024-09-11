a627d66946384ab6830e2763da58c784622b1a9e
fjchen7 <fjchen7@gmail.com>
2024-09-11 15:21:56 +0800

    process: 拆分进程控制块

--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,22 +1,49 @@
+use core::cell::RefMut;
+
+use super::{
+    pid::{KernelStack, PidHandle, PID_ALLOCATOR},
+    TaskContext,
+};
 use crate::{
-    config::{kernel_stack_position, TRAP_CONTEXT},
-    mm::{MapPermission, MemorySet, PhysPageNum, VirtAddr, KERNEL_SPACE},
+    config::TRAP_CONTEXT,
+    mm::{MemorySet, PhysPageNum, VirtAddr, KERNEL_SPACE},
+    sync::UPSafeCell,
     trap::{trap_handler, TrapContext},
 };
+use alloc::{
+    sync::{Arc, Weak},
+    vec::Vec,
+};
 
-use super::TaskContext;
-
-// 用于控制任务的结构体
+// 单个进程的控制块
+// 进程的执行状态、资源控制等元数据，都保存在该结构体中。
 pub struct TaskControlBlock {
-    pub task_status: TaskStatus,
+    // 不可变
+    pub pid: PidHandle,
+    pub kernel_stack: KernelStack,
+    // inner存放运行时可变的元数据
+    inner: UPSafeCell<TaskControlBlockInner>,
+}
+
+pub struct TaskControlBlockInner {
+    // Trap上下文存放的物理页号。它的虚拟页是地址空间的次高页。
+    pub trap_cx_ppn: PhysPageNum,
+    // 该进程的地址空间中，从0x0到用户栈结束所包含的字节。
+    // 它代表了该进程占用的内存大小（暂时不包含堆）。
+    pub base_size: usize,
+    // 进程切换或时钟中断时，要保存的上下文
     pub task_cx: TaskContext,
+    // 进程的执行状态
+    pub task_status: TaskStatus,
     // 地址空间
     pub memory_set: MemorySet,
-    // Trap上下文存放的物理页。它的虚拟页是地址空间的次高页。
-    pub trap_cx_ppn: PhysPageNum,
-    // 应用数据的大小：地址空间中，从0x0到用户栈结束所包含的字节
-    #[allow(unused)]
-    pub base_size: usize,
+    // 父进程。使用Weak，避免循环引用。
+    pub parent: Option<Weak<TaskControlBlock>>,
+    // 子进程
+    pub children: Vec<Arc<TaskControlBlock>>,
+    // 进程退出时，返回的退出码保存在这里
+    pub exit_code: i32,
+
     // 堆的底部，即堆的起始地址。数字小（堆从低地址向高地址增长）。
     pub heap_bottom: usize,
     // 堆的顶部，即堆的结束地址。数字大。
@@ -29,49 +56,69 @@ pub struct TaskControlBlock {
 pub enum TaskStatus {
     Ready,   // 准备运行
     Running, // 正在运行
-    Exited,  // 已退出
+    Zombie,  // 僵尸进程：进程已经结束，但父进程还没有回收它的资源
 }
 
-impl TaskControlBlock {
+impl TaskControlBlockInner {
     pub fn get_trap_cx(&self) -> &'static mut TrapContext {
         self.trap_cx_ppn.get_mut()
     }
-
     pub fn get_user_token(&self) -> usize {
         self.memory_set.token()
     }
+    fn get_status(&self) -> TaskStatus {
+        self.task_status
+    }
+    pub fn is_zombie(&self) -> bool {
+        self.get_status() == TaskStatus::Zombie
+    }
+}
 
-    // 解析ELF格式的二进制数据，创建一个TaskControlBlock
-    pub fn new(elf_data: &[u8], app_id: usize) -> Self {
+impl TaskControlBlock {
+    pub fn inner_exclusive_access(&self) -> RefMut<'_, TaskControlBlockInner> {
+        self.inner.exclusive_access()
+    }
+
+    pub fn getpid(&self) -> usize {
+        self.pid.0
+    }
+
+    // 解析ELF格式的二进制数据，创建一个新的进程
+    pub fn new(elf_data: &[u8]) -> Self {
         // 解析ELF，得到地址空间、用户栈顶、入口地址
         let (memory_set, user_sp, entry_point) = MemorySet::from_elf(elf_data);
-        // 得到存放TrapContext的物理页号
+        // 得到存放TrapContext的物理页号。该物理页在前面的from_elf中已经分配。
         let trap_cx_ppn = memory_set
             .translate(VirtAddr::from(TRAP_CONTEXT).into())
             .unwrap()
             .ppn();
-        let task_status = TaskStatus::Ready;
-        // 在内核地址空间中，为该程序专属的内核栈，分配物理页。
-        // 使用物理页分配器（这里的类型是Framed）。
-        let (kernel_stack_bottom, kernel_stack_top) = kernel_stack_position(app_id);
-        KERNEL_SPACE.exclusive_access().insert_framed_area(
-            kernel_stack_bottom.into(),
-            kernel_stack_top.into(),
-            MapPermission::R | MapPermission::W,
-        );
-        // 初始化TaskContext
+        // 分配新的PID
+        let pid_handle = PID_ALLOCATOR.exclusive_access().alloc();
+        // 在内核地址空间中，为该PID分配内核栈
+        let kernel_stack = KernelStack::new(&pid_handle);
+        let kernel_stack_top = kernel_stack.get_top();
+        // 初始化TaskContext，使得第一次切换到该进程时，能跳转到trap_return方法，进入它的用户态
         let task_cx = TaskContext::goto_trap_return(kernel_stack_top);
         let task_control_block = Self {
-            task_status,
-            task_cx,
-            memory_set,
-            trap_cx_ppn,
-            base_size: user_sp,
-            heap_bottom: user_sp,  // 初始化的堆为空
-            program_brk: user_sp,
+            pid: pid_handle,
+            kernel_stack,
+            inner: unsafe {
+                UPSafeCell::new(TaskControlBlockInner {
+                    trap_cx_ppn,
+                    base_size: user_sp,
+                    task_cx,
+                    task_status: TaskStatus::Ready,
+                    memory_set,
+                    parent: None,
+                    children: Vec::new(),
+                    exit_code: 0,
+                    heap_bottom: user_sp,
+                    program_brk: user_sp,
+                })
+            },
         };
-        // 初始化程序的TrapContext
-        let trap_cx = task_control_block.get_trap_cx();
+        // 在用户空间，初始化该进程的TrapContext
+        let trap_cx = task_control_block.inner_exclusive_access().get_trap_cx();
         *trap_cx = TrapContext::app_init_context(
             entry_point,
             user_sp,
@@ -85,20 +132,21 @@ impl TaskControlBlock {
     // 增加或减少堆的大小
     // 改变成功时，返回原来堆的结束位置（最高位）
     pub fn change_program_brk(&mut self, size: i32) -> Option<usize> {
-        let old_break = self.program_brk;
-        let new_brk = self.program_brk as isize + size as isize;
-        if new_brk < self.heap_bottom as isize {
+        let mut inner = self.inner_exclusive_access();
+        let old_break = inner.program_brk;
+        let new_brk = inner.program_brk as isize + size as isize;
+        if new_brk < inner.heap_bottom as isize {
             return None;
         }
-        let heap_bottom = VirtAddr(self.heap_bottom);
+        let heap_bottom = VirtAddr(inner.heap_bottom);
         let new_end = VirtAddr(new_brk as usize);
         let result = if size < 0 {
-            self.memory_set.shrink_to(heap_bottom, new_end)
+            inner.memory_set.shrink_to(heap_bottom, new_end)
         } else {
-            self.memory_set.append_to(heap_bottom, new_end)
+            inner.memory_set.append_to(heap_bottom, new_end)
         };
         if result {
-            self.program_brk = new_brk as usize;
+            inner.program_brk = new_brk as usize;
             Some(old_break)
         } else {
             None
