d82fc8d17fa461b2e298592918b35ed4f8408bcb
fjchen7 <fjchen7@gmail.com>
2024-09-19 20:41:30 +0800

    pipe: 添加用于测试的用户程序

--- user/src/bin/cat.rs
+++ user/src/bin/cat.rs
@@ -8,10 +8,11 @@ extern crate alloc;
 use user_lib::{close, open, read, OpenFlags};
 
 #[no_mangle]
-pub fn main() -> i32 {
-    let fd = open("filea\0", OpenFlags::RDONLY);
+pub fn main(argc: usize, argv: &[&str]) -> i32 {
+    assert!(argc == 2);
+    let fd = open(argv[1], OpenFlags::RDONLY);
     if fd == -1 {
-        panic!("Error occured when opening file");
+        panic!("Error occurred when opening file");
     }
     let fd = fd as usize;
     let mut buf = [0u8; 256];
@@ -20,7 +21,7 @@ pub fn main() -> i32 {
         if size == 0 {
             break;
         }
-        println!("{}", core::str::from_utf8(&buf[..size]).unwrap());
+        print!("{}", core::str::from_utf8(&buf[..size]).unwrap());
     }
     close(fd);
     0
--- user/src/bin/initproc.rs
+++ user/src/bin/initproc.rs
@@ -1,15 +1,14 @@
 #![no_std]
 #![no_main]
 
-#[macro_use]
 extern crate user_lib;
 
-use user_lib::{exec, fork, wait, yield_};
+use user_lib::{exec, fork, println, wait, yield_};
 
 #[no_mangle]
 fn main() -> i32 {
     if fork() == 0 {
-        exec("user_shell\0");
+        exec("user_shell\0", &[core::ptr::null::<u8>()]);
     } else {
         loop {
             let mut exit_code: i32 = 0;
